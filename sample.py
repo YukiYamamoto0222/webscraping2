6a82983af46695a5da7b40e87296bbed2cb196f2
Yuki Yamamoto <shinki.y.yamamoto@gmail.com>
2018-09-25 18:12:46 +0900

    add another sample code

--- sample.py
+++ sample.py
@@ -1,9 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-target_url = ''
+target_url = 'https://note.mu/daikawai/n/nc73889d6d835'
 r = requests.get(target_url)         #requestsを使って、webから取得
 soup = BeautifulSoup(r.text, 'lxml') #要素を抽出
 
-for a in soup.find_all('a'):
-      print(a.get('href'))         #リンクを表示
+for hoge in soup.find_all("a"):
+    print(hoge.get("href"))
+
+#for a in soup.find_all('a'):
+      #print(a.get('href'))         #リンクを表示
