81da8bab053300a92778954ad1fbe0fb9fd409ad
Yuki Yamamoto <shinki.y.yamamoto@gmail.com>
2018-10-03 15:25:47 +0900

    add some sample files

--- sample.py
+++ sample.py
@@ -5,8 +5,8 @@ target_url = 'https://note.mu/daikawai/n/nc73889d6d835'
 r = requests.get(target_url)         #requestsを使って、webから取得
 soup = BeautifulSoup(r.text, 'lxml') #要素を抽出
 
-for hoge in soup.find_all("a"):
-    print(hoge.get("href"))
+for hoge in soup.find_all("img"):
+    print(hoge)
 
 #for a in soup.find_all('a'):
       #print(a.get('href'))         #リンクを表示
